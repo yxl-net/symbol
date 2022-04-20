ba5c2c148b5a94047dbd73f3bfa2a377e1b51a01
yxl-net <996986842@qq.com>
2022-04-20 13:39:18 +0800

    符号数据由在代码中生成改为读取数据文件，内存表字段名使用有意义的值

--- Yxl/FormMain.Designer.cs
+++ Yxl/FormMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Yxl
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -30,42 +30,50 @@ namespace Yxl
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ts = new System.Windows.Forms.ToolStrip();
             this.tslSearch = new System.Windows.Forms.ToolStripLabel();
-            this.tstSearch = new System.Windows.Forms.ToolStripTextBox();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.dgv = new System.Windows.Forms.DataGridView();
+            this.tstFilter = new System.Windows.Forms.ToolStripTextBox();
+            this.tssFilter = new System.Windows.Forms.ToolStripSeparator();
             this.tsdCopy = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tmiCopySymbol = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmiCopyUnicode = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmiCopyHtml = new System.Windows.Forms.ToolStripMenuItem();
+            this.tssCopy = new System.Windows.Forms.ToolStripSeparator();
+            this.tslCount = new System.Windows.Forms.ToolStripLabel();
+            this.dgv = new System.Windows.Forms.DataGridView();
             this.cms = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.cmfCopy = new System.Windows.Forms.ToolStripMenuItem();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.toolStrip1.SuspendLayout();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ts.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.cms.SuspendLayout();
             this.SuspendLayout();
             // 
-            // toolStrip1
+            // ts
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ts.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ts.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tslSearch,
-            this.tstSearch,
-            this.toolStripSeparator1,
-            this.tsdCopy});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
+            this.tstFilter,
+            this.tssFilter,
+            this.tsdCopy,
+            this.tssCopy,
+            this.tslCount});
+            this.ts.Location = new System.Drawing.Point(0, 0);
+            this.ts.Name = "ts";
+            this.ts.Size = new System.Drawing.Size(800, 27);
+            this.ts.TabIndex = 0;
+            this.ts.Text = "toolStrip1";
             // 
             // tslSearch
             // 
@@ -73,25 +81,69 @@ namespace Yxl
             this.tslSearch.Size = new System.Drawing.Size(54, 24);
             this.tslSearch.Text = "搜索：";
             // 
-            // tstSearch
+            // tstFilter
             // 
-            this.tstSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tstSearch.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
-            this.tstSearch.Name = "tstSearch";
-            this.tstSearch.Size = new System.Drawing.Size(100, 27);
-            this.tstSearch.TextChanged += new System.EventHandler(this.tstSearch_TextChanged);
+            this.tstFilter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tstFilter.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tstFilter.Name = "tstFilter";
+            this.tstFilter.Size = new System.Drawing.Size(100, 27);
+            this.tstFilter.TextChanged += new System.EventHandler(this.tstFilter_TextChanged);
             // 
-            // toolStripSeparator1
+            // tssFilter
             // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            this.tssFilter.Name = "tssFilter";
+            this.tssFilter.Size = new System.Drawing.Size(6, 27);
+            // 
+            // tsdCopy
+            // 
+            this.tsdCopy.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tmiCopySymbol,
+            this.tmiCopyUnicode,
+            this.tmiCopyHtml});
+            this.tsdCopy.Image = ((System.Drawing.Image)(resources.GetObject("tsdCopy.Image")));
+            this.tsdCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsdCopy.Name = "tsdCopy";
+            this.tsdCopy.Size = new System.Drawing.Size(73, 24);
+            this.tsdCopy.Text = "复制";
+            // 
+            // tmiCopySymbol
+            // 
+            this.tmiCopySymbol.Name = "tmiCopySymbol";
+            this.tmiCopySymbol.Size = new System.Drawing.Size(224, 26);
+            this.tmiCopySymbol.Text = "符号";
+            this.tmiCopySymbol.Click += new System.EventHandler(this.tmiCopySymbol_Click);
+            // 
+            // tmiCopyUnicode
+            // 
+            this.tmiCopyUnicode.Name = "tmiCopyUnicode";
+            this.tmiCopyUnicode.Size = new System.Drawing.Size(224, 26);
+            this.tmiCopyUnicode.Text = "Unicode";
+            this.tmiCopyUnicode.Click += new System.EventHandler(this.tmiCopyUnicode_Click);
+            // 
+            // tmiCopyHtml
+            // 
+            this.tmiCopyHtml.Name = "tmiCopyHtml";
+            this.tmiCopyHtml.Size = new System.Drawing.Size(224, 26);
+            this.tmiCopyHtml.Text = "HTML实体";
+            this.tmiCopyHtml.Click += new System.EventHandler(this.tmiCopyHtml_Click);
+            // 
+            // tssCopy
+            // 
+            this.tssCopy.Name = "tssCopy";
+            this.tssCopy.Size = new System.Drawing.Size(6, 27);
+            // 
+            // tslCount
+            // 
+            this.tslCount.Name = "tslCount";
+            this.tslCount.Size = new System.Drawing.Size(18, 24);
+            this.tslCount.Text = "0";
             // 
             // dgv
             // 
             this.dgv.AllowUserToAddRows = false;
             this.dgv.AllowUserToDeleteRows = false;
             this.dgv.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(222)))), ((int)(((byte)(194)))));
             this.dgv.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
@@ -107,8 +159,16 @@ namespace Yxl
             this.Column2,
             this.Column3,
             this.Column4,
-            this.Column5,
-            this.Column6});
+            this.Column6,
+            this.Column5});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(213)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv.DefaultCellStyle = dataGridViewCellStyle4;
             this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgv.Location = new System.Drawing.Point(0, 27);
             this.dgv.MultiSelect = false;
@@ -120,15 +180,6 @@ namespace Yxl
             this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgv.Size = new System.Drawing.Size(800, 423);
             this.dgv.TabIndex = 1;
-            // 
-            // tsdCopy
-            // 
-            this.tsdCopy.Image = ((System.Drawing.Image)(resources.GetObject("tsdCopy.Image")));
-            this.tsdCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsdCopy.Name = "tsdCopy";
-            this.tsdCopy.Size = new System.Drawing.Size(73, 24);
-            this.tsdCopy.Text = "复制";
-            this.tsdCopy.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.tsdCopy_DropDownItemClicked);
             // 
             // cms
             // 
@@ -143,12 +194,11 @@ namespace Yxl
             this.cmfCopy.Name = "cmfCopy";
             this.cmfCopy.Size = new System.Drawing.Size(108, 24);
             this.cmfCopy.Text = "复制";
-            this.cmfCopy.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.tsdCopy_DropDownItemClicked);
             // 
             // Column1
             // 
-            this.Column1.DataPropertyName = "s1";
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Column1.DataPropertyName = "sSymbol";
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Column1.DefaultCellStyle = dataGridViewCellStyle3;
             this.Column1.HeaderText = "符号";
             this.Column1.MinimumWidth = 6;
@@ -158,7 +208,7 @@ namespace Yxl
             // 
             // Column2
             // 
-            this.Column2.DataPropertyName = "s2";
+            this.Column2.DataPropertyName = "sUnicode";
             this.Column2.HeaderText = "Unicode";
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
@@ -167,7 +217,7 @@ namespace Yxl
             // 
             // Column3
             // 
-            this.Column3.DataPropertyName = "s3";
+            this.Column3.DataPropertyName = "sHtml";
             this.Column3.HeaderText = "HTML实体";
             this.Column3.MinimumWidth = 6;
             this.Column3.Name = "Column3";
@@ -176,42 +226,43 @@ namespace Yxl
             // 
             // Column4
             // 
-            this.Column4.DataPropertyName = "s4";
+            this.Column4.DataPropertyName = "sType";
             this.Column4.HeaderText = "类型";
             this.Column4.MinimumWidth = 6;
             this.Column4.Name = "Column4";
             this.Column4.ReadOnly = true;
             this.Column4.Width = 125;
             // 
-            // Column5
-            // 
-            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column5.DataPropertyName = "s5";
-            this.Column5.HeaderText = "说明";
-            this.Column5.MinimumWidth = 6;
-            this.Column5.Name = "Column5";
-            this.Column5.ReadOnly = true;
-            // 
             // Column6
             // 
-            this.Column6.DataPropertyName = "s6";
+            this.Column6.DataPropertyName = "sValue";
             this.Column6.HeaderText = "值";
             this.Column6.MinimumWidth = 6;
             this.Column6.Name = "Column6";
             this.Column6.ReadOnly = true;
             this.Column6.Width = 60;
             // 
-            // Form1
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column5.DataPropertyName = "sDesc";
+            this.Column5.HeaderText = "说明";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dgv);
-            this.Controls.Add(this.toolStrip1);
-            this.Name = "Form1";
+            this.Controls.Add(this.ts);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "特殊符号小程序";
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            this.ts.ResumeLayout(false);
+            this.ts.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.cms.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -221,20 +272,25 @@ namespace Yxl
 
         #endregion
 
-        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStrip ts;
         private System.Windows.Forms.ToolStripLabel tslSearch;
-        private System.Windows.Forms.ToolStripTextBox tstSearch;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripTextBox tstFilter;
+        private System.Windows.Forms.ToolStripSeparator tssFilter;
         private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.ToolStripDropDownButton tsdCopy;
         private System.Windows.Forms.ContextMenuStrip cms;
         private System.Windows.Forms.ToolStripMenuItem cmfCopy;
+        private System.Windows.Forms.ToolStripMenuItem tmiCopySymbol;
+        private System.Windows.Forms.ToolStripMenuItem tmiCopyUnicode;
+        private System.Windows.Forms.ToolStripMenuItem tmiCopyHtml;
+        private System.Windows.Forms.ToolStripSeparator tssCopy;
+        private System.Windows.Forms.ToolStripLabel tslCount;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
     }
 }
 
---
+++ Yxl/FormMain.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Yxl
+{
+    /// <summary>
+    /// 主窗体
+    /// </summary>
+    public partial class FormMain : Form
+    {
+        #region 字段
+        /// <summary>
+        /// 内存表
+        /// </summary>
+        DataTable dt = new DataTable();
+        #endregion
+
+        #region 构造方法
+        /// <summary>
+        /// 主窗体 构造方法
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            dgv.GetType().GetProperty("DoubleBuffered", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).SetValue(dgv, true, null);//双缓冲
+            dt.DefaultView.ListChanged += (o, x) =>tslCount.Text=(o as DataView).Count.ToString();//行数改变
+            dgv.DataSource = dt;//数据绑定
+            dt.Columns.Add("sSymbol");//符号
+            dt.Columns.Add("sUnicode");//Unicode码
+            dt.Columns.Add("sHtml");//HTML实体
+            dt.Columns.Add("sType");//类型
+            dt.Columns.Add("sValue");//值
+            dt.Columns.Add("sDesc");//描述
+            foreach(string line in File.ReadAllLines("yxl"))//遍历文件行集
+            {
+                dt.Rows.Add(line.Split(new char[] { ' ' }, 6));//添加行 6个值
+            }
+            
+            string s = "㆒㆓㆔㆕㆖㆗㆘㆙㆚㆛㆜㆝㆞㆟";
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in s)
+            {
+                sb.AppendLine($"{c} \\u{((int)c).ToString("X")} &#{(int)c};    ");
+            }
+            s = sb.ToString();            
+        }
+        #endregion
+
+        #region 工具栏句柄
+        /// <summary>
+        /// 过滤框文本改变
+        /// </summary>
+        /// <param name="sender">控件</param>
+        /// <param name="e">事件参数</param>
+        private void tstFilter_TextChanged(object sender, EventArgs e)
+        {
+            dt.DefaultView.RowFilter = String.Format("sValue like '%{0}%' or sDesc like '%{0}%'", tstFilter.Text.Trim().Replace("'","''").Replace("%","[%]").Replace("*","[*]").Replace("[","[[]"));//值和描述 模糊搜索
+        }
+
+        /// <summary>
+        /// 点击复制符号菜单项
+        /// </summary>
+        /// <param name="sender">控件</param>
+        /// <param name="e">事件参数</param>
+        private void tmiCopySymbol_Click(object sender, EventArgs e)
+        {
+            try//资源异常
+            {
+                Clipboard.SetText(dgv.CurrentRow.Cells[0].Value.ToString());//设置剪贴板文本
+            }
+            catch//不报错
+            {
+            }
+        }
+
+        /// <summary>
+        /// 点击复制Unicode菜单项
+        /// </summary>
+        /// <param name="sender">控件</param>
+        /// <param name="e">事件参数</param>
+        private void tmiCopyUnicode_Click(object sender, EventArgs e)
+        {
+            try//资源异常
+            {
+                Clipboard.SetText(dgv.CurrentRow.Cells[1].Value.ToString());//设置剪贴板文本
+            }
+            catch//不报错
+            {
+            }
+        }
+
+        /// <summary>
+        /// 点击复制HTML实体菜单项
+        /// </summary>
+        /// <param name="sender">控件</param>
+        /// <param name="e">事件参数</param>
+        private void tmiCopyHtml_Click(object sender, EventArgs e)
+        {
+            try//资源异常
+            {
+                Clipboard.SetText(dgv.CurrentRow.Cells[2].Value.ToString());//设置剪贴板文本
+            }
+            catch//不报错
+            {
+            }
+        }
+        #endregion
+    }
+}
